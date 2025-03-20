8bab80c0e0fbeac564983b8ed97de7136f6c560c
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-20 11:01:37 +0100

    refactor

--- internal/server.go
+++ internal/server.go
@@ -52,7 +52,7 @@ func (s *Server) SetupRoutes() http.Handler {
 // BroadcastFocusState sends the current focus state to all connected clients
 func (s *Server) BroadcastFocusState() {
 	message := s.State.GetCurrentFocusInfo()
-	s.State.BroadcastToClients(message)
+	s.State.NotifyAllClient(message)
 }
 
 // BroadcastQuote sends a random quote to all connected clients
@@ -65,5 +65,5 @@ func (s *Server) BroadcastQuote() {
 		Quote: s.QuoteStore.GetQuote().Text,
 	}
 	
-	s.State.BroadcastToClients(message)
+	s.State.NotifyAllClient(message)
 }
